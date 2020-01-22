d25a72abc46a9fd1864ca1e202ef908c0c1eef2f
syibro98 <syibrobagus@gmail.com>
2020-01-22 19:32:37 +0700

    Penambahan Login ke Firebase

--- app/src/main/java/com/example/hutangku/LoginAct.java
+++ app/src/main/java/com/example/hutangku/LoginAct.java
@@ -1,18 +1,30 @@
 package com.example.hutangku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginAct extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     Button btnLogin, btnSignUp;
     RelativeLayout rellay1, rellay2;
+    private EditText etUsername, etPassword;
 
     Handler handler = new Handler();
     Runnable runnable = new Runnable() {
@@ -31,6 +43,10 @@ public class LoginAct extends AppCompatActivity {
         rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
         rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
 
+        mAuth = FirebaseAuth.getInstance();
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+
         handler.postDelayed(runnable, 2000); // 2000 is the timeout for the splash
 
         btnLogin = findViewById(R.id.btnLogin);
@@ -40,9 +56,53 @@ public class LoginAct extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent a = new Intent(LoginAct.this,DashboardAct.class);
+               Intent a = new Intent(LoginAct.this,DashboardAct.class);
                 startActivity(a);
                 finish();
+           }
+        });
+
+//        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (username.equals("")) {
+                    Toast.makeText(LoginAct.this, "Silahkan input username Anda",
+                            Toast.LENGTH_SHORT).show();
+
+                }else if (password.equals("")) {
+                    Toast.makeText(LoginAct.this, "Silahkan input password Anda",
+                            Toast.LENGTH_SHORT).show();
+
+                }else {
+
+                    mAuth.signInWithEmailAndPassword(username, password)
+                            .addOnCompleteListener(LoginAct.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+//                                    Log.d(TAG, "createUserWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        Toast.makeText(LoginAct.this, "Authentication success.",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+//                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(LoginAct.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    // ...
+                                }
+                            });
+
+                }
             }
         });
 
@@ -56,5 +116,11 @@ public class LoginAct extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
     }
 }
